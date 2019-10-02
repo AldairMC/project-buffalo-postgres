de219f3395ddb1e52be921196d8a4b824964c64a
Nicolas Camacho <ncamacho1@cuc.edu.co>
2019-10-02 13:58:04 -0500

    :construction: Made db connection

--- main.go
+++ main.go
@@ -1,39 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	guuid "github.com/google/uuid"
 	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-//Player define model of player
-type Player struct {
-	ID       string `json:"id"`
-	Nickname string `json:"nickname"`
-	Online   bool   `json:"online"`
+func initDB(filepath string) *slq.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	// Here we check for any db errors then exit
+	if err != nil {
+		panic(err)
+	}
+
+	// If we don't get any errors but somehow still don't get a db connection
+	// we exit as well
+	if db == nil {
+		panic("db nil")
+	}
+	return db
 }
 
-//Players define array of players
-type Players []Player
+//Post define model of player
+type Post struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Author      string `json:"author"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
+}
 
-var players Players
+//Posts define array of players
+type Posts []Post
+
+var posts Posts
 
 func generateID() string {
 	id := guuid.New()
 	return id.String()
 }
 
-func getPlayers(c echo.Context) error {
-	return c.JSON(http.StatusOK, players)
+func getPosts(c echo.Context) error {
+	return c.JSON(http.StatusOK, posts)
 }
 
-func getPlayer(c echo.Context) error {
+func getPost(c echo.Context) error {
 	id := c.Param("id")
-	for _, player := range players {
-		if player.ID == id {
-			c.JSON(http.StatusOK, player)
+	for _, post := range posts {
+		if post.ID == id {
+			c.JSON(http.StatusOK, post)
 		}
 	}
 	return c.JSON(http.StatusBadRequest, nil)
@@ -50,15 +72,27 @@ func putPlayer(c echo.Context) error {
 	return c.JSON(http.StatusBadRequest, nil)
 }
 
-func postPlayer(c echo.Context) error {
-	player := Player{}
-	err := c.Bind(&player)
+func postPost(c echo.Context) error {
+	post := Post{}
+	err := c.Bind(&post)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusUnprocessableEntity)
 	}
-	player.ID = generateID()
-	players = append(players, player)
-	return c.JSON(http.StatusCreated, players)
+	post.ID = generateID()
+	post.CreatedAt = time.Now().String()
+	posts = append(posts, post)
+	return c.JSON(http.StatusCreated, posts)
+}
+
+func deletePost(c echo.Context) error {
+	id, _ := c.Param("id")
+	for i, _ := range posts {
+		if posts[i].ID == id {
+			posts = append(posts[:id], posts[i+1]...)
+			return c.JSON(http.StatusOK, posts)
+		}
+	}
+	return c.JSON(http.StatusBadRequest, nil)
 }
 
 func main() {
@@ -68,5 +102,5 @@ func main() {
 	e.POST("/players", postPlayer)
 	e.GET("/players/:id", getPlayer)
 	e.PUT("/players/:id", putPlayer)
-	e.Start(":9000")
+	e.Start(":8080")
 }
